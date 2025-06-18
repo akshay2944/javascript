32a5f1f363836b504ab865319b86123ec5befc61
akshay2944 <akshay1082006.com>
2025-06-18 17:10:13 +0530

    add js file

---
+++ script.js
@@ -0,0 +1,13 @@
+const today=new Date()
+const dayname=today.toDateString('en-US')
+
+
+const dateelement=document.querySelector('#akshay')
+  dateelement.innerHTML=today.toDateString('en-US')
+  
+
+
+
+
+
+
